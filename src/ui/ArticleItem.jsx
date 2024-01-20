07ff649aa559fa48f49e6597356b239065758c0f
Anthony Arinze <anthonyarinze28@gmail.com>
2024-01-20 21:25:03 +0100

    Article changes

--- src/ui/ArticleItem.jsx
+++ src/ui/ArticleItem.jsx
@@ -3,13 +3,7 @@ import Microlink from "@microlink/react";
 
 const StyledMicrolink = styled(Microlink)`
   font-family: inherit;
-  width: 50%;
   border-radius: 0.42857em;
-
-  @media (min-width: 768px) {
-    /* Apply styles for medium screens and larger (md:w-[100%]) */
-    max-width: 100%;
-  }
 `;
 
 const ArticleItem = ({ url }) => {
