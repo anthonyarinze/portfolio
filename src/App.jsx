4e443cca6dc4fe675b6da68f9ed89fb84dfcc8ee
Anthony Arinze <anthonyarinze28@gmail.com>
2024-03-30 13:54:31 +0100

    Layout changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="flex h-dvh w-dvw flex-col place-items-center bg-gray-600 text-gray-400">
-      <div className="glow-border h-90 w-90 overflow-auto rounded-md bg-bg-main p-5">
+    <div className="flex h-dvh w-dvw flex-col items-center justify-center bg-gray-600 p-3 text-gray-400">
+      <div className="glow-border h-full w-full overflow-auto rounded-md bg-bg-main p-5">
         <Header />
         <Divider />
         <Body />
